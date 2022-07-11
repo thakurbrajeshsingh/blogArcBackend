d37fb280639b8bafea85cadf25e7cbbb55bf0118
thakurbrajeshsingh <brajeshkumar997@gmail.com>
2022-07-11 22:25:23 +0530

    Cors Error Handled

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import express from "express";
 import Connection from "./database/db.js";
 import dotenv from 'dotenv';
 import Router from './routes/routes.js'
+import cors from 'cors'
+import bodyParser from "body-parser";
+
+
 
 const app = express();
 
+
+app.use(cors())
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Router);
 
 
