c26d12c1ad99226f62de8284715cf3d9294e3de4
thakurbrajeshsingh <brajeshkumar997@gmail.com>
2022-07-12 08:34:26 +0530

    Password Encryption Added

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,14 +1,20 @@
 // Schema
 import User from '../model/user.js'
 
+// for password encryption
+import bcrypt from 'bcrypt'
 
 export const signupUser = async (request, response) => {
     try {
+        // const salt = await bcrypt.genSalt();
+        // const hashedPassword = await bcrypt.hash(request.body.password, salt);
+        // 10 indicates salt of length 10 ,new method no need to gensalt manually
+        const hashedPassword = await bcrypt.hash(request.body.password, 10);
         const exist = await User.findOne({ username: request.body.username });
         if (exist) {
             return response.status(401).json({ msg: 'User Already Exist' })
         }
-        const user = request.body;
+        const user = { username: request.body.username, name: request.body.name, password: hashedPassword };
         const newUser = User(user);
         await newUser.save();
         return response.status(200).json({ msg: 'signup successfull' })
